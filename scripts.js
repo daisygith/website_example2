bd1faf10c488f536e63b33efc1722053f22946fe
daisygith <daisygith@users.noreply.github.com>
2024-07-26 14:54:02 +0200

    added script for mouse

--- scripts.js
+++ scripts.js
@@ -1,4 +1,25 @@
 //copy logos
-
 let copy = document.querySelector(".logos-slide").cloneNode(true);
 document.querySelector(".logos").appendChild(copy);
+
+//cursors
+const cursorTag = document.querySelector("div.cursors");
+const ball = cursorTag.querySelector("div");
+const ballMessage = cursorTag.querySelector("div span");
+
+const message=document.querySelectorAll("a[data-hover]");
+
+document.addEventListener("mousemove", function (event){
+    ball.style.left = event.pageX + "px";
+    ball.style.top = event.pageY + "px";
+})
+
+message.forEach(text => {
+    text.addEventListener("mouseover", function (){
+        ballMessage.classList.add("visible");
+        ballMessage.innerHTML = text.getAttribute("data-hover");
+    });
+    text.addEventListener("mouseout", function (){
+        ballMessage.classList.remove("visible");
+    });
+})
