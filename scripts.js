e4b0f2fc625f1773f134c4224e804f6b5ac0efe5
daisygith <daisygith@users.noreply.github.com>
2024-07-29 16:13:36 +0200

    changed cursor depends on box

--- scripts.js
+++ scripts.js
@@ -4,22 +4,57 @@ document.querySelector(".logos").appendChild(copy);
 
 //cursors
 const cursorTag = document.querySelector("div.cursors");
+
 const ball = cursorTag.querySelector("div");
 const ballMessage = cursorTag.querySelector("div span");
-
+// const img = document.querySelectorAll(".logo-work");
+// const specialFrame = document.querySelectorAll(".special-frame.logo");
+//
 const message=document.querySelectorAll("a[data-hover]");
-
+let keyword = "active";
+//
 document.addEventListener("mousemove", function (event){
     ball.style.left = event.pageX + "px";
     ball.style.top = event.pageY + "px";
 })
 
 message.forEach(text => {
-    text.addEventListener("mouseover", function (){
+    text.addEventListener("mouseover", function (el){
+        cursorTag.classList.add("active");
         ballMessage.classList.add("visible");
+        let img = el?.currentTarget?.querySelector(".logo-work");
+        let specialFrame = el?.currentTarget?.querySelector(".special-frame.logo");
+        img.classList.add("transform");
+        specialFrame.classList.add("transform");
         ballMessage.innerHTML = text.getAttribute("data-hover");
+
     });
-    text.addEventListener("mouseout", function (){
+    text.addEventListener("mouseout", function (el){
         ballMessage.classList.remove("visible");
+        cursorTag.classList.remove("active");
+        let img = el?.currentTarget?.querySelector(".logo-work");
+        let specialFrame = el?.currentTarget?.querySelector(".special-frame.logo");
+
+        img.classList.remove("transform");
+        specialFrame.classList.remove("transform");
+
     });
 })
+
+
+
+// img.forEach(elem => {
+//     elem.classList.add("transform");
+// })
+
+
+// ballMessage.forEach(elem => {
+//     elem.addEventListener("mouseover",function (){
+//         img.classList.add("transform");
+//         specialFrame.classList.add("transform");
+//     });
+//     elem.addEventListener("mouseout", function (){
+//         img.classList.remove("transform");
+//         specialFrame.classList.remove("transform");
+//     });
+// })
