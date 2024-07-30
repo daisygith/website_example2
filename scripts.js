c0789790dd493430c9ed0dd6e6d213dc17a9e515
daisygith <daisygith@users.noreply.github.com>
2024-07-30 08:34:08 +0200

    copy box

--- scripts.js
+++ scripts.js
@@ -2,13 +2,17 @@
 let copy = document.querySelector(".logos-slide").cloneNode(true);
 document.querySelector(".logos").appendChild(copy);
 
+//copy box
+let copyBox = document.querySelector(".result").cloneNode(true);
+document.querySelector(".opinions-slide").appendChild(copyBox);
+
+
+
 //cursors
 const cursorTag = document.querySelector("div.cursors");
 
 const ball = cursorTag.querySelector("div");
 const ballMessage = cursorTag.querySelector("div span");
-// const img = document.querySelectorAll(".logo-work");
-// const specialFrame = document.querySelectorAll(".special-frame.logo");
 //
 const message=document.querySelectorAll("a[data-hover]");
 let keyword = "active";
@@ -41,20 +45,3 @@ message.forEach(text => {
     });
 })
 
-
-
-// img.forEach(elem => {
-//     elem.classList.add("transform");
-// })
-
-
-// ballMessage.forEach(elem => {
-//     elem.addEventListener("mouseover",function (){
-//         img.classList.add("transform");
-//         specialFrame.classList.add("transform");
-//     });
-//     elem.addEventListener("mouseout", function (){
-//         img.classList.remove("transform");
-//         specialFrame.classList.remove("transform");
-//     });
-// })
