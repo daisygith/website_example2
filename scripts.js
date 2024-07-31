c45a7acf8c83ea9042b0b5739e512ab338d9371c
daisygith <daisygith@users.noreply.github.com>
2024-07-31 10:12:40 +0200

    added function to arrow to image box team

--- scripts.js
+++ scripts.js
@@ -7,7 +7,6 @@ let copyBox = document.querySelector(".result").cloneNode(true);
 document.querySelector(".opinions-slide").appendChild(copyBox);
 
 
-
 //cursors
 const cursorTag = document.querySelector("div.cursors");
 
@@ -15,7 +14,6 @@ const ball = cursorTag.querySelector("div");
 const ballMessage = cursorTag.querySelector("div span");
 //
 const message=document.querySelectorAll("a[data-hover]");
-let keyword = "active";
 //
 document.addEventListener("mousemove", function (event){
     ball.style.left = event.pageX + "px";
@@ -30,8 +28,8 @@ message.forEach(text => {
 
         let img = el?.currentTarget?.querySelector(".logo-work");
         let specialFrame = el?.currentTarget?.querySelector(".special-frame.logo");
-
         let changeColor = el?.currentTarget?.querySelector(".content-img.team");
+
         img.classList.add("transform");
         specialFrame.classList.add("transform");
         changeColor.classList.add("transform");
@@ -51,5 +49,68 @@ message.forEach(text => {
     });
 })
 
+//scroll gallery
+// let scrollContainer = document.querySelector(".scroll-images");
+// scrollContainer.addEventListener("wheel", (el) => {
+
+//     el.preventDefault();
+//     scrollContainer.scrollLeft += el.deltaY;
+//     scrollContainer.style.scrollBehavior = "auto";
+// });
+//
 
 
+// document.addEventListener("DOMContentLoaded", function () {
+
+const scrollContainer = document.querySelector(".scroll-images");
+const scrollLength = scrollContainer.scrollWidth - scrollContainer.clientWidth;
+
+let left = document.getElementById("left");
+let right = document.getElementById("right");
+
+const leftBtn = document.querySelector(".left");
+const rightBtn = document.querySelector(".right");
+
+    function checkScroll(qualifiedName) {
+        const currentScroll = scrollContainer.scrollLeft;
+        if(currentScroll === 0){
+            leftBtn.setAttribute("disabled", "true");
+            rightBtn.removeAttribute("disabled");
+        } else if (currentScroll === scrollLength){
+            rightBtn.setAttribute("disabled", "true");
+            leftBtn.removeAttribute("disabled");
+        } else {
+            leftBtn.removeAttribute("disabled");
+            rightBtn.removeAttribute("disabled");
+        }
+    }
+
+    scrollContainer.addEventListener("scroll",checkScroll);
+    window.addEventListener("resize",checkScroll);
+    checkScroll();
+
+right.addEventListener("click", () => {
+    scrollContainer.style.scrollBehavior = "smooth";
+    scrollContainer.scrollLeft += 380;
+});
+left.addEventListener("click", () => {
+    scrollContainer.style.scrollBehavior = "smooth";
+    scrollContainer.scrollLeft -= 380;
+});
+
+    // function leftScroll() {
+    //     scrollContainer.scrollBy({
+    //         left: -200,
+    //         behavior: "smooth"
+    //     });
+    // }
+    // function rightScroll() {
+    //     scrollContainer.scrollBy({
+    //         left: -200,
+    //         behavior: "smooth"
+    //     });
+    // }
+    //
+    // leftBtn.addEventListener("click", leftScroll);
+    // rightBtn.addEventListener("click", rightScroll);
+// })
