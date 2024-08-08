85226a24a51f33199773a3e6f5ca7cae3a15b905
daisygith <daisygith@users.noreply.github.com>
2024-08-08 11:57:49 +0200

    added function to headlines sections

--- scripts.js
+++ scripts.js
@@ -1,3 +1,40 @@
+//scroll section
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+let navLinksFoot = document.querySelectorAll('footer a');
+
+console.log(navLinks);
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offSet = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec?.getAttribute('id');
+        console.log(id);
+
+        if (top >= offSet && top < offSet + height) {
+
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                // id.classList.add("active");
+                document.querySelector('header nav a[href*= ' + id + ']')?.classList.add('active');
+            });
+            navLinksFoot.forEach(links => {
+                links.classList.remove('active');
+                // id.classList.add("active");
+                document.querySelector('footer a[href*= ' + id + ']')?.classList.add('active');
+            });
+
+        }
+
+    });
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+}
+
+
 //copy logos
 let copy = document.querySelector(".logos-slide").cloneNode(true);
 document.querySelector(".logos").appendChild(copy);
@@ -13,7 +50,7 @@ const cursorTag = document.querySelector("div.cursors");
 const ball = cursorTag.querySelector("div");
 const ballMessage = cursorTag.querySelector("div span");
 //
-const message = document.querySelectorAll("a[data-hover]");
+const message = document.querySelectorAll("a[data-hover].content");
 //
 document.addEventListener("mousemove", function (event) {
     ball.style.left = event.pageX + "px";
@@ -21,34 +58,34 @@ document.addEventListener("mousemove", function (event) {
 })
 
 message.forEach(text => {
-        text.addEventListener("mouseover", function (el) {
-            cursorTag.classList.add("active");
-            ballMessage.classList.add("visible");
-            ballMessage.innerHTML = text.getAttribute("data-hover");
+    text.addEventListener("mouseover", function (el) {
+        cursorTag.classList.add("active");
+        ballMessage.classList.add("visible");
+        ballMessage.innerHTML = text.getAttribute("data-hover");
 
-            let img = el?.currentTarget?.querySelector(".logo-work");
-            let specialFrame = el?.currentTarget?.querySelector(".special-frame.logo");
-            let changeColor = el?.currentTarget?.querySelector(".content-img.team");
+        let img = el?.currentTarget?.querySelector(".logo-work");
+        let specialFrame = el?.currentTarget?.querySelector(".special-frame.logo");
+        let changeColor = el?.currentTarget?.querySelector(".content-img.team");
 
-            img?.classList.add("transform");
-            specialFrame?.classList.add("transform");
-            changeColor?.classList.add("transform");
+        img?.classList.add("transform");
+        specialFrame?.classList.add("transform");
+        changeColor?.classList.add("transform");
 
-        });
-        text.addEventListener("mouseout", function (el) {
-            ballMessage.classList.remove("visible");
-            cursorTag.classList.remove("active");
 
-            let img = el?.currentTarget?.querySelector(".logo-work");
-            let specialFrame = el?.currentTarget?.querySelector(".special-frame.logo");
-            let changeColor = el?.currentTarget?.querySelector(".content-img.team");
+    });
+    text.addEventListener("mouseout", function (el) {
+        ballMessage.classList.remove("visible");
+        cursorTag.classList.remove("active");
 
-            img?.classList.remove("transform");
-            specialFrame?.classList.remove("transform");
-            changeColor?.classList.remove("transform");
-        });
-    }
-)
+        let img = el?.currentTarget?.querySelector(".logo-work");
+        let specialFrame = el?.currentTarget?.querySelector(".special-frame.logo");
+        let changeColor = el?.currentTarget?.querySelector(".content-img.team");
+
+        img?.classList.remove("transform");
+        specialFrame?.classList.remove("transform");
+        changeColor?.classList.remove("transform");
+    });
+})
 
 const scrollContainer = document.querySelector(".scroll-images");
 const scrollLength = scrollContainer.scrollWidth - scrollContainer.clientWidth;
@@ -105,3 +142,29 @@ openQuestion.forEach((element) => {
         }
     })
 })
+
+
+//
+const elementsSlide = document.querySelector('.slideText');
+console.log(elementsSlide);
+const observer = new IntersectionObserver(entries => {
+    elementsSlide.classList.toggle('slide-top', entries[0].isIntersecting);
+});
+
+observer.observe(elementsSlide);
+
+document.addEventListener("DOMContentLoaded", function () {
+    const elementsSlide = document.querySelectorAll('.slideText');
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("slide-top");
+            } else {
+                entry.target.classList.remove("slide-top");
+            }
+        });
+    },);
+    elementsSlide.forEach(el => {
+        observer.observe(el);
+    });
+});
